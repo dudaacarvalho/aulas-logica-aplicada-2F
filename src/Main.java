6d13fe881f365c57cbefb32eb93db5ee687b6675
dudaacarvalho <eduarda.mecap@gmail.com>
2023-05-18 08:31:58 -0300

    Falamos sobre Orientação a Objetos, onde vimos como declarar, intanciar, e definir objetos, além de acionar o comportamento, como "falar". Usamos Adão e Eva como exemplo, onde foi executado os métodos e o resultado apareceu na tela.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Aula de Orientação à Objetos
+
+        // Declarar objetos
+        Pessoa adao;
+       // clase "Pessoa" que deu origem ao objeto: "Adão"
+
+        // Instanciar objetos
+        adao = new Pessoa();
+        Pessoa eva = new Pessoa();
+
+        // Definição do formato
+        adao.nome = "Adão";
+        adao.sobrenome = "Silva";
+        eva.nome = "Eva";
+        eva.sobrenome = "Silva";
+
+        // Acionar comportamento - fazer o adao falar
+        adao.falar();
+        eva.falar();
+
+
+
+
+
     }
 }
